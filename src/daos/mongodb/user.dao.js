e614c3b0b81bb34fc2ca89a06f3c6028823535cb
Jorge Montesinos <jorge.osmar@laboratoria.la>
2023-09-03 19:41:45 -0500

    added methods to users dao and service

--- src/daos/mongodb/user.dao.js
+++ src/daos/mongodb/user.dao.js
@@ -1,8 +1,38 @@
 import MongoDao from "./mongo.dao.js";
 import { userModel } from "./models/user.model.js";
+import { createHash, isValidPassword } from "../../utils.js";
 
 export default class UserDao extends MongoDao {
   constructor() {
     super(userModel);
+  }
+
+  async register(user) {
+    const { email, password } = user;
+    const existUser = await this.getByEmail(email);
+    if (!existUser) {
+      const newUser = await this.create({
+        ...user,
+        password: createHash(password),
+      });
+      if (!newUser) return false;
+      else return newUser;
+    }else return false;
+  }
+
+  async getByEmail(email) {
+    const user = await this.model.findOne({ email })
+    if(user) return user;
+    else return false
+  }
+
+  async login(user) {
+    const { email, password } = user;
+    const userExists = await this.getByEmail(email);
+    if (userExists) {
+      const passwordValid = isValidPassword(user, password);
+      if (!passwordValid) return false
+      else return userExists;
+    }
   }
 }
--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,9 +1,32 @@
 import Services from './class.services.js';
+import { pkg } from "jsonwebtoken";
+import "dotenv/config"
 
 import UserDao from "../daos/mongodb/user.dao.js";
+
+const SECRET_KEY = process.env.JWT_SECRET;
 
 export default class UserServices extends Services {
     constructor() {
         super(new UserDao())
+    }
+
+    #generateToken(user) {
+      const payload = {
+        id: user._id,
+      }
+      return pkg.sign(payload, SECRET_KEY, { expiresIn: '3m' })
+    }
+
+    async register(user) {
+     return await UserDao.register(user)
+    }
+
+    async login(user) {
+      const userExists = await UserDao.login(user)
+      if(userExists) {
+        const token = this.#generateToken(userExists)
+        return { token, user: userExists }
+      }else return false
     }
 }
