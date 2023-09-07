332028fb5806034ec82643eb28a0ce1354e28339
Jorge Montesinos <jorge.osmar@laboratoria.la>
2023-09-06 22:26:27 -0500

    added middleware errorHandler and config.js

--- src/daos/mongodb/connection.js
+++ src/daos/mongodb/connection.js
@@ -1,9 +1,9 @@
 import { connect } from "mongoose";
-import "dotenv/config";
+import config from '../../config.js';
 
 try {
-  console.log("connection", process.env.MONGO_LOCAL_URL);
-  await connect(process.env.MONGO_LOCAL_URL);
+  console.log("connection", config.MONGO_LOCAL_URL);
+  await connect(config.MONGO_LOCAL_URL);
   console.log("Conectado a la base de datos de MongoDB");
 } catch (error) {
   console.log(error);
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import "./daos/mongodb/connection.js";
 import morgan from "morgan";
 import MainRouter from './routes/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
-import "dotenv/config";
+import config from './config.js';
 
 const app = express();
 const mainRouter = new MainRouter();
@@ -16,6 +16,6 @@ app
   .use('/api', mainRouter.getRouter())
   .use(errorHandler);
 
-const PORT = process.env.PORT || 8080;
+const PORT = config.PORT || 8080;
 
 app.listen(PORT, () => console.log(`SERVER UP ON PORT ${PORT}`));
