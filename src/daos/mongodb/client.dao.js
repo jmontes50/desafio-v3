937e6b34ec4b326c3aa6b2a6735ad135ffc497c1
Jorge Montesinos <osmarmontes50@gmail.com>
2023-10-03 22:01:54 -0500

    advance

--- src/daos/mongodb/client.dao.js
+++ src/daos/mongodb/client.dao.js
@@ -1,8 +1,39 @@
 import MongoDao from "./mongo.dao.js";
 import { clientModel } from "./models/client.model.js";
+import { createHash, isValidPassword } from "../../utils.js";
 
 export default class ClientDao extends MongoDao {
   constructor() {
     super(clientModel);
+  }
+
+  async register(client) {
+    const { email, password } = client;
+    const existClient = await this.getByEmail(email);
+    if (!existClient) {
+      const newClient = await this.create({
+        ...client,
+        password: createHash(password),
+      });
+      if (!newClient) return false;
+      return newClient;
+    }
+    return false;
+  }
+
+  async getByEmail(email) {
+    const client = await this.model.findOne({ email });
+    if (client) return client;
+    return false;
+  }
+
+  async login(client) {
+    const { email, password } = client;
+    const clientExists = await this.getByEmail(email);
+    if (clientExists) {
+      const passwordValid = isValidPassword(client, password);
+      if (!passwordValid) return false;
+      return clientExists;
+    }
   }
 }
--- src/services/client.services.js
+++ src/services/client.services.js
@@ -1,4 +1,7 @@
+import pkg from "jsonwebtoken";
 import Services from "./class.services.js";
+
+const SECRET_KEY = process.env.JWT_SECRET;
 
 import ClientDao from "../daos/mongodb/client.dao.js";
 
@@ -7,5 +10,31 @@ const clientDao = new ClientDao();
 export default class ClientServices extends Services {
   constructor() {
     super(clientDao);
+  }
+
+  #generateToken(client) {
+    const payload = {
+      id: client._id,
+      name: client.name,
+      email: client.email,
+    };
+    return pkg.sign(payload, SECRET_KEY, { expiresIn: "3m" });
+  }
+
+  async register(client) {
+    try {
+      return await clientDao.register(client);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async login(client) {
+    const clientExists = await clientDao.login(client);
+    if (clientExists) {
+      const token = this.#generateToken(clientExists);
+      return { token };
+    }
+    return false;
   }
 }
